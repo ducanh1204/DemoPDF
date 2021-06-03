f13e986772ddc91d29bec5baf1ba55cb32cd1db4
ducanh1204 <51355767+ducanh1204@users.noreply.github.com>
2021-06-03 21:34:35 +0700

    a

--- app/src/main/java/com/example/demopdf/PDFActivity.java
+++ app/src/main/java/com/example/demopdf/PDFActivity.java
@@ -2,13 +2,27 @@ package com.example.demopdf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+import java.io.File;
 
 public class PDFActivity extends AppCompatActivity {
 
+    PDFView pdfView;
+    String path;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdfactivity);
+        pdfView = findViewById(R.id.pdfView);
+
+        Intent intent = getIntent();
+        path = intent.getStringExtra("path");
+        File file = new File(path);
+        pdfView.fromFile(file).load();
     }
 }
